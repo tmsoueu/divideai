e4bcb141884340b9959be3bd5f82ae22ba5147d7
thiagomaldonadoc@gmail.com <thiagomaldonadoc@gmail.com>
2025-05-26 01:35:28 -0300

    Update logo and enhance login view layout with new design elements

--- src/components/resources.py
+++ src/components/resources.py
@@ -4,4 +4,10 @@
 ICON = 'icon.png'  # Este ícone deve estar em assets_dir
 
 # === TELA DE LOGIN ===
-LOGO = 'https://i.imgur.com/x3KdLr5.png'  # Para ser substituído depois por "logo.png"
+LOGO = 'https://i.imgur.com/kAb1SO4.png'  # Para ser substituído depois por "logo.png"
+
+# === CORES ===
+BRANCO = '#f6f6f6'
+AMARELO = '#ff8800'
+AZUL_ESCURO = '#010a18'
+AZUL_CLARO = '#01132e'    
--- src/views/login_view.py
+++ src/views/login_view.py
@@ -1,7 +1,7 @@
 """Módulo da tela de login do DivideAI."""
 
 import flet as ft
-from components.resources import LOGO
+from components.resources import *
 
 class LoginView(ft.View):
     """
@@ -25,20 +25,59 @@ class LoginView(ft.View):
         self.page = page
         self.route = '/'
         self.padding = ft.padding.all(0)
+        
         self.controls = [
             ft.Container(
-                expand=True,
                 width=self.page.width,
+                height=self.page.height,
                 gradient=ft.LinearGradient(
                     begin=ft.alignment.top_left,
                     end=ft.alignment.bottom_right,
-                    colors=['#010a18', '#01132e'],
+                    colors=[AZUL_ESCURO, AZUL_CLARO],
                 ),
-                content=ft.Image(
-                    src=LOGO,
-                    width=100,
-                    fit=ft.ImageFit.CONTAIN
+                content=ft.Column(
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    #spacing=40,
+                    controls=[
+                        ft.Image(
+                            src=LOGO,
+                            width=270,
+                        ),
+                        ft.Container(
+                            content=ft.ElevatedButton(
+                                width=page.width * 0.5,
+                                bgcolor='#f6f6f6',
+                                color='#010a18',
+                                style=ft.ButtonStyle(
+                                    padding=ft.padding.symmetric(horizontal=20, vertical=10),
+                                    shape=ft.RoundedRectangleBorder(radius=8),
+                                ),
+                                content=ft.Row(
+                                    alignment=ft.MainAxisAlignment.CENTER,
+                                    vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                                    controls=[
+                                        ft.Image(
+                                            src='https://img.icons8.com/color/48/google-logo.png',
+                                            width=20,
+                                            height=20,
+                                        ),
+                                        ft.Text('Entrar com o Google', weight=ft.FontWeight.BOLD),
+                                    ],
+                                ),
+                                on_click=lambda e: print('Login com Google clicado')
+                            ),
+                        ),
+                        # Rodapé com direitos autorais
+                        ft.Container(
+                            #padding=ft.padding.only(bottom=20),
+                            content=ft.Text(
+                                "© 2026 DivideAI. Made with ❤ by TM.",
+                                size=10,
+                                color=BRANCO,
+                            )
+                        )
+                    ]
                 )
             )
         ]
-        
